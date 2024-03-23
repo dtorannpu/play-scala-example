c49577611d6f23aff39739a9085c9a40698e5690
dtorannpu <dtorannpu@gmail.com>
2024-03-23 11:24:50 +0900

    Articleの作成とID取得のテスト追加

--- test/repositories/ArticleRepositorySpec.scala
+++ test/repositories/ArticleRepositorySpec.scala
@@ -1,17 +1,27 @@
 package repositories
 
 import org.scalatest.*
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.matchers.should.Matchers.should
+import org.scalatest.matchers.should.Matchers._
 import org.scalatestplus.play.*
+
+import scala.language.postfixOps
 
 class ArticleRepositorySpec extends H2RepositorySpec {
   private lazy val articleRepository = inject[ArticleRepository]
-  
-  "" in {
-    var result = articleRepository.all()
-    whenReady(articleRepository.all()) { articles =>
-      articles should have size 0
+
+  "ArticleRepository" should {
+    "all" in {
+      whenReady(articleRepository.all()) { articles =>
+        articles should have size 0
+      }
+    }
+    "create and find" in {
+      whenReady(articleRepository.create("タイトル", "内容")) { a =>
+        whenReady(articleRepository.findById(a.id)) { article =>
+          article shouldBe defined
+          article.value.title should equal ("タイトル")
+        }
+      }
     }
   }
 }
